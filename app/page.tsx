6de76a8c04b898191f55f4f192609491cb46f090
Qinbeans <rybeats@live.com>
2024-03-14 17:32:46 -0700

    added jon's name

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="items-center justify-between px-56 py-4">
       <section className=" px-4 py-4 grid place-content-center bg-white/15 rounded-2xl border-white border frosted h-fit">
-        <h1 className="text-center font-bold underline">Mr. Crabs' Bakery</h1>
+        <h1 className="text-center font-bold underline">Jon's Bakery</h1>
         <Carousel className="w-[50dvh] h-[50dvh]"/>
         <p className="text-center">We have the best buns in town!</p>
       </section>
